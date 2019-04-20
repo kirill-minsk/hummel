b8a70305f3949a9b02a022571c9d78eb1c9d7bbf
kirill minsk <programkirill@gmail.com>
2019-04-20 15:40:17 +0300

    teeeeeeeeest

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-tests files
-w
-
+  <div class="col-sm-12">
+    <label class="control-label"><?php echo $entry_rating; ?></label>
+    &nbsp;&nbsp;&nbsp; <?php echo $entry_bad; ?>&nbsp;
+    <input type="radio" name="rating" value="1" />
+    &nbsp;
+    <input type="radio" name="rating" value="2" />
+    &nbsp;
+    <input type="radio" name="rating" value="3" />
+    &nbsp;
+    <input type="radio" name="rating" value="4" />
+    &nbsp;
+    <input type="radio" name="rating" value="5" />
+    &nbsp;<?php echo $entry_good; ?></div>
+</div>
